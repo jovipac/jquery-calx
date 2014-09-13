33b3f9c4588aee1a51740bb3f8e2680ffe47009c
Ikhsan Agustian <ikhsan017@gmail.com>
2014-09-14 06:03:57 +0700

    add new lite formula container

---
+++ php-build/include/calx/calculate.js
@@ -0,0 +1,11 @@
+calculate : function(){
+    return this.each(function(){
+        var sheetIdentifier = $(this).attr('data-calx-identifier');
+        //console.log(sheetIdentifier);
+
+        if(sheetIdentifier && typeof(calx.sheetRegistry[sheetIdentifier]) == 'undefined'){
+            calx.sheetRegistry[sheetIdentifier].calculate();
+            calx.sheetRegistry[sheetIdentifier].renderComputedValue();
+        }
+    });
+}
